240ad4522bca0db3c08c631d7c90f4e80bf304da
Stiven Camara <stiven.camara@babel.es>
2020-07-14 12:56:09 +0100

    Change parameter name

--- Office2Pdf/IConverter.cs
+++ Office2Pdf/IConverter.cs
@@ -4,6 +4,6 @@ namespace Office2Pdf
 {
     public interface IConverter
     {
-        void Convert(string sourcePath, string targetPath, bool isPdf);
+        void Convert(string sourcePath, string targetPath, bool isPdfA);
     }
 }
